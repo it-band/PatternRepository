3b2afa7c7cd496ce5b650c79ca10b69ca6deea2d
Denis Novogrodsky <denis.novogrodsky@ids-group.co.uk>
2020-09-02 15:44:10 +0300

    update service collection extension

--- src/ITB.Repository.EntityFrameworkCore/RepositoryEntityFrameworkCoreServiceCollectionExtensions.cs
+++ src/ITB.Repository.EntityFrameworkCore/RepositoryEntityFrameworkCoreServiceCollectionExtensions.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Linq;
+using System.Reflection;
 using ITB.Repository.Abstraction;
+using ITB.Shared.Domain;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 
@@ -7,7 +10,7 @@ namespace ITB.Repository.EntityFrameworkCore
 {
     public static class RepositoryEntityFrameworkCoreServiceCollectionExtensions
     {
-        public static IServiceCollection AddRepository(this IServiceCollection services)
+        public static IServiceCollection AddRepository(this IServiceCollection services, params Assembly[] assemblies)
         {
             if (services == null)
                 throw new ArgumentNullException(nameof(services));
@@ -16,7 +19,36 @@ namespace ITB.Repository.EntityFrameworkCore
             services.TryAddTransient(typeof(IReadRepository<>), typeof(ReadRepository<>));
             services.TryAddTransient<IUnitOfWork, UnitOfWork>();
 
+            if(assemblies != null && assemblies.Length > 0)
+            {
+                var allTypes = assemblies
+                    .Where(a => !a.IsDynamic)
+                    .Distinct()
+                    .SelectMany(a => a.DefinedTypes)
+                    .ToArray();
+
+                var openTypes = new[]
+                    {
+                    typeof(IQueryableFilter<>)
+                };
+
+                foreach (var type in openTypes.SelectMany(openType => allTypes
+                    .Where(t => t.IsClass
+                        && !t.IsAbstract
+                        && t.AsType().ImplementsGenericInterface(openType))))
+                {
+                    // use try add to avoid double-registration
+                    services.TryAddTransient(type.AsType());
+                }
+            }
+
             return services;
         }
+
+        private static bool ImplementsGenericInterface(this Type type, Type interfaceType)
+      => type.IsGenericType(interfaceType) || type.GetTypeInfo().ImplementedInterfaces.Any(@interface => @interface.IsGenericType(interfaceType));
+
+        private static bool IsGenericType(this Type type, Type genericType)
+            => type.GetTypeInfo().IsGenericType && type.GetGenericTypeDefinition() == genericType;
     }
 }
