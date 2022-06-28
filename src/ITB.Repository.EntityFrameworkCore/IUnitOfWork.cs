b31558353d9db05daeb97ef0d6ecbe1e8380572d
Mikhail Shyshla <mikhail.shyshla@ids-group.co.uk>
2022-06-28 17:14:55 +0300

    update to .net 6

---
+++ src/ITB.Repository.EntityFrameworkCore/IUnitOfWork.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ITB.Repository.EntityFrameworkCore
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        Task<int> SaveChanges(CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// Changes the State of all tracked Entities to Detached so that they are no longer tracked.
+        /// This keeps the performance linear for commands that perform database operations recursively, such as batch operations.
+        /// </summary>
+        void DetachAllEntities();
+
+        // Set timeout for command
+        void SetCommandTimeout(TimeSpan timeSpan);
+
+
+        /// <summary>
+        /// If the entity is not attached to the DB context, it means it was obtained by the read-only context, and is attached as Unchanged.
+        /// Otherwise it means it was either obtained by the write context or already attached - either way it should already by in the correct state.
+        /// This should be used before creating a relationship between an entity that was potentially obtained by the read-only context
+        /// and an entity that will be added/updated by the write context. Only suitable when the entity is not already tracked,
+        /// as there is otherwise a chance that a different model of the same entity is already tracked and an error would be thrown.
+        /// </summary>
+        void AttachAsUnchangedIfMissing<TEntity>(TEntity entity) where TEntity : class;
+
+        DbContext Context();
+
+        Task<IDbContextTransaction> BeginTransaction();
+
+        Task CommitTransaction();
+
+        Task RollbackTransaction();
+    }
+}
--- src/ITB.Repository.EntityFrameworkCore/UnitOfWork.cs
+++ src/ITB.Repository.EntityFrameworkCore/UnitOfWork.cs
@@ -1,8 +1,9 @@
 ﻿using System;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
-using ITB.Repository.Abstraction;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
 
 namespace ITB.Repository.EntityFrameworkCore
 {
@@ -14,6 +15,47 @@ namespace ITB.Repository.EntityFrameworkCore
         public UnitOfWork(DbContext dbContext)
         {
             _dbContext = dbContext;
+        }
+
+        public void DetachAllEntities()
+        {
+            _dbContext.ChangeTracker.Clear();
+        }
+
+        public void SetCommandTimeout(TimeSpan timeSpan)
+        {            
+            _dbContext.Database.SetCommandTimeout(timeSpan);
+        }
+
+        public DbContext Context()
+        {
+            return _dbContext;
+        }
+
+        public async Task<IDbContextTransaction> BeginTransaction()
+        {
+            return await _dbContext.Database.BeginTransactionAsync();
+        }
+
+        public async Task CommitTransaction()
+        {
+            await _dbContext.Database.CommitTransactionAsync();
+        }
+
+        public async Task RollbackTransaction()
+        {
+            await _dbContext.Database.RollbackTransactionAsync();
+        }
+
+        /// <inheritdoc />
+        public void AttachAsUnchangedIfMissing<TEntity>(TEntity entity) where TEntity : class
+        {
+            // if entity is not already attached to this context
+            if (entity != null && _dbContext.ChangeTracker.Entries().All(x => x.Entity != entity))
+            {
+                // attach if not already attached, and set state to Unchanged
+                _dbContext.Entry(entity).State = EntityState.Unchanged;
+            }
         }
 
         public void Dispose(bool disposing)
@@ -44,5 +86,6 @@ namespace ITB.Repository.EntityFrameworkCore
         {
             return _dbContext.SaveChangesAsync(cancellationToken);
         }
+
     }
 }
